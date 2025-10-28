2bfafc10efc869aad9f9c1a414c10c1d72dc2b70
Faewulf <ngolamaz3@gmail.com>
2025-10-28 17:54:02 +0700

    # Fix: Standalone right-click event, prevent further conflict.

--- common/src/main/java/xyz/faewulf/piggyback/mixin/PlayerMixin.java
+++ common/src/main/java/xyz/faewulf/piggyback/mixin/PlayerMixin.java
@@ -1,12 +1,19 @@
 package xyz.faewulf.piggyback.mixin;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.faewulf.piggyback.event.performRiding;
 
-@Mixin(Player.class)
+@Mixin(value = Player.class, priority = 1)
 public abstract class PlayerMixin extends LivingEntity {
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
@@ -23,4 +30,11 @@ public abstract class PlayerMixin extends LivingEntity {
 //            if(this.getFirstPassenger() != null)
 //                this.getFirstPassenger().stopRiding();
 //    }
+
+    @Inject(method = "interactOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;", ordinal = 0), cancellable = true)
+    private void Piggyback$interactOnInject(Entity entityToInteractOn, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        if((Object) this instanceof Player player) {
+            performRiding.run(this.level(), player, hand, entityToInteractOn, null);
+        }
+    }
 }
--- fabric/src/main/java/xyz/faewulf/piggyback/event_handler/performRiding.java
+++ fabric/src/main/java/xyz/faewulf/piggyback/event_handler/performRiding.java
@@ -5,6 +5,6 @@ import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 public class performRiding {
 
     public static void register() {
-        UseEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> xyz.faewulf.piggyback.event.performRiding.run(world, player, hand, entity, hitResult)));
+        //UseEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> xyz.faewulf.piggyback.event.performRiding.run(world, player, hand, entity, hitResult)));
     }
 }
