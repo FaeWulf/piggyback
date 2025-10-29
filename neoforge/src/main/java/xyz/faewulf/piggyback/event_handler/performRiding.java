df2123389f9380a360911a265060e7887a28f676
Faewulf <ngolamaz3@gmail.com>
2025-10-29 17:05:18 +0700

    ~ Change: No right-click event on neoforge.

--- neoforge/src/main/java/xyz/faewulf/piggyback/event_handler/performRiding.java
+++ neoforge/src/main/java/xyz/faewulf/piggyback/event_handler/performRiding.java
@@ -10,9 +10,9 @@ import xyz.faewulf.piggyback.Constants;
 public class performRiding {
     @SubscribeEvent
     public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
-        InteractionResult interactionResult = xyz.faewulf.piggyback.event.performRiding.run(event.getLevel(), event.getEntity(), event.getHand(), event.getTarget(), null);
-
-        if (interactionResult.consumesAction())
-            event.setCanceled(true);
+//        InteractionResult interactionResult = xyz.faewulf.piggyback.event.performRiding.run(event.getLevel(), event.getEntity(), event.getHand(), event.getTarget(), null);
+//
+//        if (interactionResult.consumesAction())
+//            event.setCanceled(true);
     }
 }
